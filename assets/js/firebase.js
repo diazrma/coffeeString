2e633bcf44a4d20fbf7b93ac034f9f81e0a6019e
Rodrigo <rodrigo@hibrido.com.br>
2022-02-16 13:11:49 -0300

    test

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -8,29 +8,29 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
 
 
-// import config from "../../dotenv.json" assert { type: "json" };
-
-// const firebaseConfig = {
-//   apiKey: config.apiKey,
-//   authDomain: config.authDomain,
-//   projectId: config.authDomain,
-//   databaseURL: config.databaseURL,
-//   storageBucket: config.storageBucket,
-//   messagingSenderId: config.messagingSenderId,
-//   appId: config.appId,
-//   measurementId: config.measurementId,
-// };
+import config from "../../dotenv.json" assert { type: "json" };
 
 const firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    projectId: process.env.PROJECTID,
-    databaseURL: process.env.DATABASEURL,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.APPID,
-    measurementId: process.env.MEASUREMENTID,
-  };
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.authDomain,
+  databaseURL: config.databaseURL,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId,
+};
+
+// const firebaseConfig = {
+//     apiKey: process.env.APIKEY,
+//     authDomain: process.env.AUTHDOMAIN,
+//     projectId: process.env.PROJECTID,
+//     databaseURL: process.env.DATABASEURL,
+//     storageBucket: process.env.STORAGEBUCKET,
+//     messagingSenderId: process.env.MESSAGINGSENDERID,
+//     appId: process.env.APPID,
+//     measurementId: process.env.MEASUREMENTID,
+//   };
 
 const app = initializeApp(firebaseConfig);
 
